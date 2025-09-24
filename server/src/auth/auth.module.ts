c378603f8b3e9ace02feacc854cddf2ea4808f09
Nada <lime.x.na@gmail.com>
2025-09-24 13:26:52 +0300

    Implement login auth with mock db and jwt

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,6 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UserModule } from "src/user/user.module";
+import { jwtConstants } from "./constants";
+import { JwtModule } from "@nestjs/jwt";
 
-@Module({})
-export class AuthModule {
-  
-}
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "60s" },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService]
+})
+export class AuthModule {}
